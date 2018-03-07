465bd72a010d5896f45515c901347887bb85ca9b
Samin Salam <samins95@gmail.com>
2018-03-06 22:02:38 -0500

    more startscreen changes

--- Assets/Pool Billiard Table/Scripts/menuScript.cs
+++ Assets/Pool Billiard Table/Scripts/menuScript.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class menuScript : MonoBehaviour {
 
 	public Canvas loginMenu;
+	public Canvas userMenu;
 	//public Button login; //login button
 	public Button startText; //play as guest
 	public Button scores; //database interaction
@@ -17,21 +18,34 @@ public class menuScript : MonoBehaviour {
 	void Start () {
 
 		loginMenu = loginMenu.GetComponent<Canvas> ();
+		userMenu = userMenu.GetComponent<Canvas> ();
 		startText = startText.GetComponent<Button> ();
 		scores = scores.GetComponent<Button> ();
 		loginMenu.enabled = false;
+		userMenu.enabled = false;
 
 	}
 
 	public void LoginPress () {
 		loginMenu.enabled = true;
+		userMenu.enabled = false;
 		startText.enabled = false;
 		scores.enabled = false; 
 	
 	}
 
+	public void userPress () {
+		loginMenu.enabled = false;
+		userMenu.enabled = true;
+		startText.enabled = false;
+		scores.enabled = false; 
+
+	}
+
+
 	public void NoPress () {
 		loginMenu.enabled = false;
+		userMenu.enabled = false;
 		startText.enabled = true;
 		scores.enabled = true; 
 
